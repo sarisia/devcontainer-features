bcb4c1b0c47c8bc7029fd839037a08f469dd7c4d
Sarisia <git@sarisia.cc>
2026-01-12 16:49:39 +0000

    fix claude-code

--- .devcontainer/features/claude-code/install.sh
+++ .devcontainer/features/claude-code/install.sh
@@ -2,5 +2,11 @@
 
 set -euo pipefail
 
+
 su ${_REMOTE_USER} -c "curl -fsSL https://claude.ai/install.sh | bash" 2>&1
-ln -sf /claude-code/.claude.json /home/${_REMOTE_USER}/.claude.json
+
+# remove ~/.claude and use bind mount from host instead
+rm -rf /home/${_REMOTE_USER}/.claude
+ln -sf /.claude /home/${_REMOTE_USER}
+
+ln -sf /.claude.json /home/${_REMOTE_USER}/.claude.json
