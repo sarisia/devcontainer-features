c73ac4c87386ae924a15655b2e621e9253ccf920
tfu <tfu@fibergate.co.jp>
2026-01-29 07:38:44 +0000

    opencode: fix bin symlink is not created

--- .devcontainer/features/opencode/install.sh
+++ .devcontainer/features/opencode/install.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 
 su ${_REMOTE_USER} -c "curl -fsSL https://opencode.ai/install | bash" 2>&1
 
+# opencode does not make symlink to ~/.local/bin (which claude code does)
+su ${_REMOTE_USER} -c "mkdir -p /home/${_REMOTE_USER}/.local/bin" 2>&1
+ln -sf /home/${_REMOTE_USER}/.opencode/bin/opencode /home/${_REMOTE_USER}/.local/bin
+
 # remove ~/.claude and use bind mount from host instead
 rm -rf /home/${_REMOTE_USER}/.local/share/opencode
 su ${_REMOTE_USER} -c "mkdir -p /home/${_REMOTE_USER}/.local/share" 2>&1
